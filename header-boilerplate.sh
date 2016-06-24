e8da1622d7d466e730278b70d2a09c4a62a33048
poolpog <poolpog@users.noreply.github.com>
2016-06-24 12:44:35 -0400

    Create header-boilerplate.sh

---
+++ header-boilerplate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#===============================================================================
+#         FILE: path/to/file.sh
+#        USAGE: $0 <arg1> [<arg2>] etc
+#  DESCRIPTION: Does this thing and that thing
+#               Requires
+#                - This requirement
+#                - That requirement
+#       AUTHOR: Jeffrey Silverman <jeff@blispay.com>
+#    COPYRIGHT: Copyright (c) 2016, person/company
+#      VERSION: 0.1.0
+#      CREATED: YYYY-MM-DD
+#    CHANGELOG: as required
+#===============================================================================
