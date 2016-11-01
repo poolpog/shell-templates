3598460a37831b32d1e5d6cc30756e09600918cd
poolpog <poolpog@users.noreply.github.com>
2016-11-01 13:12:14 -0400

    Update header-boilerplate.sh

--- header-boilerplate.sh
+++ header-boilerplate.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 #===============================================================================
-#         FILE: path/to/file.sh
-#        USAGE: $0 <arg1> [<arg2>] etc
+#         FILE: /path/to/file.sh
+#        USAGE: See ${USAGE} below.
 #  DESCRIPTION: Does this thing and that thing
 #               Requires
 #                - This requirement
 #                - That requirement
 #       AUTHOR: Jeffrey Silverman <jeffrey.d.silverman@gmail.com>
 #    COPYRIGHT: Copyright (c) 2016, Jeffrey D. Silverman
-#      VERSION: 0.1.0
-#      CREATED: YYYY-MM-DD
-#    CHANGELOG: as required
+#    REVISIONS: 0.1.0-YYYY-MM-DD
+#               [ - changelog item 1 ]
+#               [ - changelog item 2 ]
 #===============================================================================
+USAGE="USAGE: $0 <arg1> [<arg2>]"
